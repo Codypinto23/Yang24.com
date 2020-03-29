8f8dc8be9a776e83833a0c5d1264725242508c41
Cody Pinto <codypinto23@gmail.com>
2020-03-29 10:03:20 -0700

    add in Yang Video

--- yang24/src/App.js
+++ yang24/src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BackgroundVideo from "./components/video/BackgroundVideo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <BackgroundVideo
+              loadingImage={
+                  "https://images.pexels.com/photos/3380967/pexels-photo-3380967.jpeg?cs=srgb&dl=red-pagoda-temple-3380967.jpg&fm=jpg"
+              }
+              videoUrl={"https://turnkey-images.sfo2.digitaloceanspaces.com/yang-speech.mp4"}
+              minHeight={"220px"}
+              maxHeight={"1600px"}
+              videoHeight={"100vh"}
+              isMobile={true}
+              contentToPresent={
+                  <div
+                      style={{
+                          height: "100%",
+                          width: "100%",
+                          minHeight: "220px",
+                          maxHeight: "1600px",
+                          margin: "auto",
+                          position: "relative"
+                      }}
+                  >
+                      <div style={{ padding: "142px 24px 24px 24px" }}>
+                          <h1
+                              style={{
+                                  marginTop: "24px",
+                                  fontWeight: "800",
+                                  color: "#ffffff",
+                                  fontSize: "34px",
+                                  fontFamily:
+                                      "Circular, -apple-system, BlinkMacSystemFont, Roboto",
+                                  lineHeight: "36px",
+                                  letterSpacing: "-1px"
+                              }}
+                          >
+                              Yang will return for 2024
+                          </h1>
+                      </div>
+                  </div>
+              }
+          />
+      </div>
   );
 }
 
